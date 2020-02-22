00ef133a4501c84b62c5db5c102e4b981eb37865
Matthew Roseman <mroseman95@gmail.com>
2020-02-21 21:45:29 -0500

    Created boilerplate react code in client directory
    Includes setting up webpack and docker

---
+++ client/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import { hot } from 'react-hot-loader/root';
+
+class App extends Component {
+  render() {
+    return (
+      <h1>Hello World</h1>
+    );
+  }
+}
+
+export default hot(App);
---
+++ webpack.config.js
@@ -0,0 +1,39 @@
+const path = require("path");
+const webpack = require("webpack");
+
+module.exports = {
+  entry: path.join(__dirname, "client/index.js"),
+  mode: "development",
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: "babel-loader",
+        options: { presets: ["@babel/env"] }
+      },
+      {
+        test: /\.css$/,
+        use: ["style-loader", "css-loader"]
+      },
+      {
+        test: /\.(png|jpg|svg|gif)$/,
+        loader: "file-loader"
+      }
+    ]
+  },
+  resolve: { extensions: ["*", ".js", ".jsx"] },
+  output: {
+    path: path.resolve(__dirname, "client/dist/"),
+    publicPath: "/dist/",
+    filename: "bundle.js"
+  },
+  devServer: {
+    contentBase: path.join(__dirname, "client/public/"),
+    port: 3000,
+    publicPath: "http://localhost:3000/",
+    hotOnly: true,
+    disableHostCheck: true
+  },
+  plugins: [new webpack.HotModuleReplacementPlugin()]
+};
