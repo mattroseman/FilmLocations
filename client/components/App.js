9021820cb89dbc64ead5e046c0830235a814fbb4
Matthew Roseman <mroseman95@gmail.com>
2020-02-23 19:10:26 -0500

    update markers on map when the map is moved

--- client/components/App.js
+++ client/components/App.js
@@ -12,21 +12,38 @@ class App extends Component {
     this.state = {
       map: {
         center: [41.5, -81.6864795],
-        zoom: 14
+        zoom: 14,
+        markers: []
       },
-      markers: []
+      showingLocations: []
     };
 
     this.map = React.createRef();
   }
 
   componentDidMount() {
+    this.updateMapMarkers();
+  }
+
+  handleMapMoveEnd() {
+    this.updateMapMarkers();
+  }
+
+  /*
+   * updateMapMarkers gets the new bounds for the Map component, and gets locations in those bounds, then adds markers to the map
+   */
+  updateMapMarkers() {
     const map = this.map.current;
     if (map !== null) {
       const bounds = map.leafletElement.getBounds();
       const southWest = [bounds._southWest.lat, bounds._southWest.lng];
       const northEast = [bounds._northEast.lat, bounds._northEast.lng];
-      this.getLocations(southWest, northEast);
+      this.getLocations(southWest, northEast)
+        .then((filmLocations) => {
+          for (const filmLocation of filmLocations) {
+            this.plotLocation(filmLocation);
+          }
+        });
     }
   }
 
@@ -34,34 +51,43 @@ class App extends Component {
    * getLocations queries the backend for all locations in the given bounds
    * params southWest: [lat, lon] the lat lon coordinates for the south west corner of the bounds to search for locations in
    * params northEast: [lat, lon] the lat lon coordinates for the north east corner of the bounds to search for locations in
-   * @return: a list of lat lon coordinates that are all film locations within the given bounds
+   * @return: [{locationString: 'name of location', locationPoint: [lat, lon]}] an array of location objects
    */
   getLocations(southWest, northEast) {
-    fetch(`http://localhost:5000/film-clusters?swlat=${southWest[0]}&swlon=${southWest[1]}&nelat=${northEast[0]}&nelon=${northEast[1]}`)
-      .then((response) => {
-        return response.json();
-      }).then((response) => {
-        for (const location of response) {
-          this.plotLocation(location);
-        }
-      });
+    return new Promise((resolve, reject) => {
+      fetch(`http://localhost:5000/film-clusters?swlat=${southWest[0]}&swlon=${southWest[1]}&nelat=${northEast[0]}&nelon=${northEast[1]}`)
+        .then((response) => {
+          resolve(response.json());
+        })
+        .catch((err) => {
+          reject(err);
+        });
+    });
   }
 
   /*
    * plotLocation takes a location and plots it on the map
-   * @params location: { "locationString": "name of location", "locationPoint": [lat, lon] } an object containing location name and it's coordinates
+   * @params filmLocation: { "locationString": "name of location", "locationPoint": [lat, lon] } an object containing location name and it's coordinates
    */
-  plotLocation(location) {
-    this.setState({
-      markers: [...this.state.markers, {
-        name: location.locationString,
-        coordinate: location.locationPoint
-      }]
-    });
+  plotLocation(filmLocation) {
+    const newMarker = {
+      name: filmLocation.locationString,
+      coordinate: filmLocation.locationPoint
+    };
+
+    // only add this new marker if there isn't already a marker for the given location
+    if (this.state.showingLocations.indexOf(filmLocation.locationString) < 0) {
+      this.setState({
+        map: {
+          markers: [...this.state.map.markers, newMarker]
+        },
+        showingLocations: [...this.state.showingLocations, filmLocation.locationString]
+      });
+    }
   }
 
   render() {
-    const markers = this.state.markers.map((marker) => {
+    const markers = this.state.map.markers.map((marker) => {
       return (
         <Marker key={marker.name} position={marker.coordinate}>
           <Popup>
@@ -73,7 +99,12 @@ class App extends Component {
 
     return (
       <div id="map-container">
-        <Map center={this.state.map.center} zoom={this.state.map.zoom} ref={this.map}>
+        <Map
+          ref={this.map}
+          center={this.state.map.center}
+          zoom={this.state.map.zoom}
+          onMoveend={() => this.handleMapMoveEnd()}
+        >
           <TileLayer
             attribution='&copy; <a href="https://stadiamaps.com/">Stadia Maps</a>, &copy; <a href="https://openmaptiles.org/">OpenMapTiles</a> &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors'
             url="https://tiles.stadiamaps.com/tiles/alidade_smooth_dark/{z}/{x}/{y}{r}.png"
