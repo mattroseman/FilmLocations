bb29dbe01a9c1a79eeb1ef65e35d3b33d3ec5999
Matthew Roseman <mroseman95@gmail.com>
2020-03-03 23:17:19 -0500

    Added geocachPrefixes field to locations
    Filter by this object with each geohash prefix as a seperate value which
    is faster then filtering with a regex on a single geohash field

---
+++ scripts/split-geohash.js
@@ -0,0 +1,40 @@
+const connectToDatabase = require('../lib/db.js');
+
+const { Location } = require('../lib/models.js');
+
+connectToDatabase();
+
+(async () => {
+  const locationIds = await Location.find({'geohash': {$exists: true}}).distinct('_id');
+
+  const numLocations = locationIds.length;
+  let locationsProcessed = 0;
+
+  console.log(`${numLocations} locations with geohashes`);
+
+  for (const locationId of locationIds) {
+    const location = await Location.findOne({'_id': locationId});
+    if (!location.geohash) {
+      continue;
+    }
+    const geohash = location.geohash;
+
+    location.geohashPrefixes = {
+      9: geohash.slice(0, 9),
+      8: geohash.slice(0, 8),
+      7: geohash.slice(0, 7),
+      6: geohash.slice(0, 6),
+      5: geohash.slice(0, 5),
+      4: geohash.slice(0, 4),
+      3: geohash.slice(0, 3),
+      2: geohash.slice(0, 2),
+      1: geohash.slice(0, 1)
+    }
+
+    await location.save();
+
+    locationsProcessed++;
+
+    console.log(`${locationsProcessed}/${numLocations} geohash: ${geohash}`);
+  }
+})();
