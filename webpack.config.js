694b32a3a1d52b1d10d0f9012428ba68c6feb82d
Matthew Roseman <mroseman95@gmail.com>
2020-04-22 22:00:49 -0400

    fixed public path discrepency breaking webpack hot reload

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
   resolve: { extensions: ["*", ".js", ".jsx"] },
   output: {
     path: path.resolve(__dirname, "client/dist/"),
-    publicPath: "/dist/",
+    publicPath: "/",
     filename: "bundle.js"
   },
   devServer: {
